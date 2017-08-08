c4bb0b4f0e30e48de91d74bf5df34840917e748c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-08-08 23:28:31 +1200

    PEP8 updates

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ import time
 import sys
 import os
 
-class Ui(QtWidgets.QMainWindow):
-	"""
-	Main class that loads and runs the ``main.ui``.
-	"""
-	def __init__(self):
-		super(Ui, self).__init__()
-		uic.loadUi('gui'+os.sep+'main.ui', self)
 
-		# showing the app gui to user
-		self.show()
+class Ui(QtWidgets.QMainWindow):
+    """
+    Main class that loads and runs the ``main.ui``.
+    """
+
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('gui' + os.sep + 'main.ui', self)
+
+        # showing the app gui to user
+        self.show()
 
 
 if __name__ == '__main__':
-	app = QtWidgets.QApplication(sys.argv)
-	window = Ui()
-	sys.exit(app.exec_())
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    sys.exit(app.exec_())
