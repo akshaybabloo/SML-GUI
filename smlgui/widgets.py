d943e12c69d960ba3b574abb0ca98f43e2fbe287
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-08-17 16:06:35 +1200

    TabWidget now uses NumpyModel for 3D array

--- smlgui/widgets.py
+++ smlgui/widgets.py
@@ -2,40 +2,31 @@
 Custom widgets for the GUI
 """
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 
 class TabWidget(QtWidgets.QTabWidget):
-    def __init__(self, parent=None):
+    def __init__(self, narray, parent=None):
         super(TabWidget, self).__init__(parent)
 
         tablist = []
-        tablabellist = []
         layoutlist = []
         self.tablelist = []
         self.setMinimumHeight(150)
         self.setMinimumWidth(400)
 
-        headerlist = ['ID', 'Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Difficulty', 'Statistics', 'Date Added',
-                      'Added By', 'Date Modified']
-
-        num_tab_widgets = 10
+        num_tab_widgets = narray.shape[0]
 
         for i in range(num_tab_widgets):
             tablist.append(QtWidgets.QWidget())
             self.addTab(tablist[i], str('Sample %s' % i))
-            # tablabellist.append(QtWidgets.QLabel('title'))
-            self.tablelist.append(QtWidgets.QTableWidget())
+            self.tablelist.append(QtWidgets.QTableView())
             setattr(self, 'Table%d' % i, self.tablelist[i])
             layoutlist.append(QtWidgets.QVBoxLayout())
 
-            self.tablelist[i].setColumnCount(len(headerlist))
-            self.tablelist[i].setHorizontalHeaderLabels(headerlist)
-            self.tablelist[i].setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
-            self.tablelist[i].setSelectionBehavior(QtWidgets.QTableWidget.SelectRows)
-            self.tablelist[i].setSelectionMode(QtWidgets.QTableWidget.SingleSelection)
+            model = NumpyModel(narray[i])
+            self.tablelist[i].setModel(model)
 
-            # layoutlist[i].addWidget(tablabellist[i])
             layoutlist[i].addWidget(self.tablelist[i])
             tablist[i].setLayout(layoutlist[i])
 
