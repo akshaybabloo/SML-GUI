1650130bd80982798e4ecc6e8f5d363072f0e89f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-16 12:02:48 +1300

    Anaconda 5 issuse updated

--- sml.cmd
+++ sml.cmd
@@ -1,1 +1,11 @@
-python -c "from smlgui.main import main; main()"
+@ECHO OFF
+
+IF EXIST %USERPROFILE%\Anaconda3\NUL (
+    echo Anaconda 3 available. Activating root environment.
+    activate root
+    echo Running SML GUI.
+    python -c "from smlgui.main import main; main()"
+) ELSE (
+    echo Anaconda 3 not available. Software might break.
+    python -c "from smlgui.main import main; main()"
+)
