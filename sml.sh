898d7c2d98aed58cfeb626fca040e3d40458eea1
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-10-16 13:21:48 +1300

    Anaconda 5 shell script updated for linux

--- sml.sh
+++ sml.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-python -c "from smlgui.main import main; main()"
+if [-d "~/anaconda3"]; then
+    echo "Anaconda 3 available. Activating root environment."
+    source activate root
+    echo "Running SML GUI."
+    python -c "from smlgui.main import main; main()"
+    else
+    echo "Anaconda 3 not available. Software might break."
+    python -c "from smlgui.main import main; main()"
+fi
